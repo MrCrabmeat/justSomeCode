e5382803d48691520ac85ece9517eddde284f079
MrCrabmeat <ashton29th@gmail.com>
2025-09-16 10:13:33 +0100

    begun quicksort

---
+++ Number of sweets practice.js
@@ -0,0 +1,5 @@
+//Write an algorithm to allow the user to enter an integer number for the number of paper
+//bags, and a second integer (which must be greater than the first) for the number of sweets.
+//The program then tells the user whether it is possible to put an odd number of sweets in
+//each bag.
+
