3a4f13b577b8587a3500a36d51a11c8ddc8de1f7
Ashton <ashton29th@gmail.com>
2025-09-19 10:33:59 +0100

    Just installed pycharm cause vscode users fell into the "free" propaganda.

--- SortingAlgorithmsJS_UI.js
+++
@@ -1,7 +0,0 @@
-//this project is supposed to be a *near* replica of
-//The SortingAlgorithmsUI.py file found in this repo
-//this is to help with learning OOP and implementing
-//Sorting algorithms which might be used in later files.
-
-let a = 12
-console.log(a)
